0be8b440f7ea6b8ce51e3bed86dbbcc603eed82f
happynote3966 <01ssrmikururudevice01@gmail.com>
2019-11-13 23:04:11 +0900

    add items api wrapper

--- src/wrapper/Virtuoso.py
+++ src/wrapper/Virtuoso.py
@@ -16,7 +16,7 @@ class Virtuoso:
         pass
 
 
-    def _v2_users(self, page, per_page):
+    def _users(self, page, per_page):
         param = {"page": str(page), "per_page": str(per_page)}
         response = self.__get("/api/v2/users", param)
         if response.status_code == 200:
@@ -24,20 +24,42 @@ class Virtuoso:
         else:
             return None
 
-    def v2_users(self, page, per_page):
+    def users(self, page, per_page):
         json_data = self._v2_users(page, per_page)
         print(json_data)
         for user in json_data:
             print(user["name"])
 
 
+    def comments_comment(self, comment_id):
+        pass
+    
+    def _comments_comment(self, comment_id):
+        pass
+
+    def items_item_comments(self, item_id):
+        pass
+
+    def _items_item_comments(self, item_id):
+        pass
+
+    def _items(self, page, per_page, query):
+        param = {"page": str(page), "per_page": str(per_page), "query": str(query)}
+        response = self.__get("/api/v2/items", param=param)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
+    def items(self, query):
+        json_data = self._items(1, 100, query)
+        print(json_data)
 
 
 def main():
     v = Virtuoso()
-    v.v2_users(1,20)
-
-    
+    v.items("python")  
 
 if __name__ == '__main__':
     main()
